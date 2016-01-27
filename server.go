7f2b1c389227f43a0b3bb55e3f19437ff2404846
Jon Gjengset <jon@thesquareplanet.com>
2016-01-27 15:45:15 -0500

    Add server with basic functionality

---
+++ server.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/sha3"
+)
+
+type question struct {
+	Text string `json:"text"`
+	Inst bool   `json:"by_instructor"`
+	By   string `json:"author_nic"`
+}
+
+type room struct {
+	mx    sync.Mutex
+	qs    []question
+	icode string
+	scode string
+}
+
+var rmx sync.Mutex
+var rooms map[string]*room
+
+func main() {
+	rooms = make(map[string]*room)
+	router := gin.Default()
+
+	router.GET("/poll/:slug/:code", func(c *gin.Context) {
+		rmx.Lock()
+		defer rmx.Unlock()
+
+		r, ok := rooms[c.Param("slug")]
+		if !ok {
+			c.String(http.StatusNotFound, "That room does not exist.")
+			return
+		}
+
+		if c.Param("code") == r.scode {
+			c.String(http.StatusForbidden, "This page is only available to instructors.")
+			return
+		}
+		if c.Param("code") != r.icode {
+			c.String(http.StatusUnauthorized, "Wrong code given for this room.")
+			return
+		}
+
+		since, err := strconv.Atoi(c.DefaultQuery("since", "0"))
+		if err != nil || since < 0 {
+			c.String(http.StatusBadRequest, "Non-integer 'since' given")
+			return
+		}
+
+		if since >= len(r.qs) {
+			c.JSON(http.StatusNoContent, []question{})
+			return
+		}
+
+		c.JSON(http.StatusOK, r.qs[since:])
+	})
+
+	router.GET("/room/:slug/:code", func(c *gin.Context) {
+		rmx.Lock()
+		defer rmx.Unlock()
+		r, ok := rooms[c.Param("slug")]
+		if !ok {
+			h := sha3.Sum512([]byte(c.Param("code")))
+			scode := hex.EncodeToString(h[:])[0:8]
+			r = &room{
+				qs: []question{{
+					Text: fmt.Sprintf("Room created. Student code is '%s'", scode),
+					Inst: true,
+					By:   "<master>",
+				}},
+				icode: c.Param("code"),
+				scode: scode,
+			}
+			rooms[c.Param("slug")] = r
+		}
+
+		if c.Param("code") == r.icode {
+			c.String(http.StatusOK, "Authenticated as instructor.\nStudent code is '%s'.", r.scode)
+		} else if c.Param("code") == r.scode {
+			c.String(http.StatusOK, "Authenticated as student.")
+		} else {
+			c.String(http.StatusUnauthorized, "Wrong code given for this room.")
+		}
+	})
+
+	router.POST("/room/:slug/:code", func(c *gin.Context) {
+		rmx.Lock()
+		defer rmx.Unlock()
+
+		r, ok := rooms[c.Param("slug")]
+		if !ok {
+			c.String(http.StatusNotFound, "That room does not exist.")
+			return
+		}
+
+		if c.Param("code") != r.icode && c.Param("code") != r.scode {
+			c.String(http.StatusUnauthorized, "Wrong code given for this room.")
+			return
+		}
+
+		q := c.PostForm("question")
+		if strings.TrimSpace(q) == "" {
+			c.String(http.StatusBadRequest, "Question was empty.")
+			return
+		}
+
+		nick := c.DefaultPostForm("nick", "student")
+		r.qs = append(r.qs, question{
+			Text: q,
+			Inst: c.Param("code") == r.icode,
+			By:   nick,
+		})
+		c.String(http.StatusOK, "Question was posted.")
+	})
+
+	// Listen and server on 0.0.0.0:8080
+	router.Run(":8080")
+}
